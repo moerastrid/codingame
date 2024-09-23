a61bdbcbc6321f6f9ef560187e782a4e90e31588
moerastrid@gmail.com <moerastrid@gmail.com>
2024-09-23 19:58:30 +0200

    small debug

--- Solution.java
+++ Solution.java
@@ -42,33 +42,32 @@ class Maze implements Cloneable {
         if (pos <= 9) {
             return (char)(pos + '0');
         }
-        return ((char)(pos + 'A'));
+        return ((char)(pos - 10 + 'A'));
     }
 
     private void virus(int _i, int _j, int pos) {
-        System.err.println("info virus i:" + _i + ", j:" + _j + ", pos: " + pos);
-        System.err.println("info1 map[i]:" + this.map[_i]);
+        // System.err.println("info virus i:" + _i + ", j:" + _j + ", pos: " + pos);
+        // System.err.println("info1 map[i]:" + this.map[_i]);
         // set this one
         
-        // StringBuilder tempstr = new StringBuilder(map[_i]);
-        // char ch = getPos(pos);
-        // tempstr.setCharAt(_j, ch);
-        // this.map[_i] = new String(tempstr.toString());
-        System.err.println("info2 map[i]:" + this.map[_i]);
+        StringBuilder tempstr = new StringBuilder(map[_i]);
+        char ch = getPos(pos);
+        tempstr.setCharAt(_j, ch);
+        this.map[_i] = new String(tempstr.toString());
+        // System.err.println("info2 map[i]:" + this.map[_i]);
 
         // check the one up
         if (_i != 0) {
-            System.err.println("info map[i-1]:" + this.map[_i - 1]);
             char temp = this.map[_i - 1].charAt(_j);
             switch (temp) {
                 case '#':
                     break ;
                 
                 case '.':
-                    // StringBuilder string = new StringBuilder(map[_i - 1]);
-                    // char c = getPos(pos);
-                    // string.setCharAt(_j, c);
-                    // this.map[_i - 1] = new String(string.toString());
+                    StringBuilder string = new StringBuilder(map[_i - 1]);
+                    char c = getPos(pos);
+                    string.setCharAt(_j, c);
+                    this.map[_i - 1] = new String(string.toString());
                     virus(_i - 1, _j, pos + 1);
                     break ;
                     
@@ -78,39 +77,72 @@ class Maze implements Cloneable {
         }
 
         // check the one down
-        // if (_i < h) {
-        //     char temp = this.map[_i + 1].charAt(_j);
-        //     switch (temp) {
-        //         case '#':
-        //             break ;
+        if (_i < h) {
+            char temp = this.map[_i + 1].charAt(_j);
+            switch (temp) {
+                case '#':
+                    break ;
                 
-        //         case '.' :
-        //             StringBuilder string = new StringBuilder(map[_i + 1]);
-        //             char c = getPos(pos);
-        //             string.setCharAt(_j, c);
-        //             this.map[_i + 1] = new String(string.toString());
-        //             virus(_i + 1, _j, pos + 1);
-        //             break;
+                case '.' :
+                    StringBuilder string = new StringBuilder(map[_i + 1]);
+                    char c = getPos(pos);
+                    string.setCharAt(_j, c);
+                    this.map[_i + 1] = new String(string.toString());
+                    virus(_i + 1, _j, pos + 1);
+                    break ;
 
-        //         default:
-        //             break;
-        //     }
-        // }
+                default:
+                    break ;
+            }
+        }
 
         // check the one left
+        if (_j != 0) {
+            char temp = this.map[_i].charAt(_j - 1);
+            switch (temp) {
+                case '#':
+                    break ;
+                
+                case '.':
+                    StringBuilder string = new StringBuilder(map[_i]);
+                    char c = getPos(pos);
+                    string.setCharAt(_j - 1, c);
+                    this.map[_i] = new String(string.toString());
+                    virus(_i, _j - 1, pos + 1);
+                    break ;
+
+                default:
+                    break ;
+            }
+        }
 
         // check the one right
+        if (_j < w - 1) {
+            char temp = this.map[_i].charAt(_j + 1);
+            switch (temp) {
+                case '#':
+                    break ;
+                
+                case '.':
+                    StringBuilder string = new StringBuilder(map[_i]);
+                    char c = getPos(pos);
+                    string.setCharAt(_j + 1, c);
+                    this.map[_i] = new String(string.toString());
+                    virus(_i, _j + 1, pos + 1);
+                    break ;
+
+                default:
+                    break ;
+            }
+        }
     }
 
     public void activateVirus(Maze input) {
         for(int i = 0; i < h; i++) {
-            String row = new String();
-
             for (int j = 0; j < w; j++) {
                 if (input.map[i].charAt(j) == 'S')
                     virus(i, j, 0);
             }
-            map[i] = new String(row);
         }
     }
 }
@@ -131,12 +163,12 @@ class Solution {
         }
         for (int i = 0; i < h; i++) {
             String ROW = in.nextLine();
-            System.err.println("row: " + ROW);
             input.map[i] = ROW;
         }
 
         solution = (Maze)input.clone();
-        System.err.println("solution map[0]: " + solution.map[0]);
+        // System.err.println("solution map[0]: " + solution.map[0]);
+
         solution.activateVirus(input);
 
         System.err.println("input: ");
